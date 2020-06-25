64e36c8ec854f59e4766ea0b042c8201ca59aa03
Rodrigo Banci <rblima0@gmail.com>
2020-06-25 12:48:11 -0300

    change preview to show genre name

--- src/pages/Discover/types.ts
+++ src/pages/Discover/types.ts
@@ -1,8 +1,10 @@
 import { DiscoverState } from '../../store/modules/discover/types'
+import { GenreState } from '../../store/modules/genre/types'
 
 export interface DiscoverProps {
   loadDiscoverRequest(page?: number, genre?: number): void
   discover: DiscoverState
+  genre: GenreState
   match: {
     params: {
       genreId: number
