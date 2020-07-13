1d5f89ab4fa44ffa33c5b9332ac9f152fc5a8b60
Rodrigo Banci <rblima0@gmail.com>
2020-07-13 18:26:11 -0300

    create entity for discover

---
+++ src/entities/Discover/types.ts
@@ -0,0 +1,28 @@
+export interface DiscoverPayload {
+  page?: number
+  genre?: number
+}
+
+export interface DiscoverResponse {
+  page: number
+  results: DiscoverResult[]
+  total_pages: number
+  total_results: number
+}
+
+export interface DiscoverResult {
+  adult: boolean
+  backdrop_path: string | undefined
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string | undefined
+  release_date: Date
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
