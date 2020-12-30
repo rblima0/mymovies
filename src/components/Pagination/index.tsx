d49170ce2c2f6e4a6bb5938a1650bca27a07a3bf
Rodrigo Banci <rblima0@gmail.com>
2020-12-30 19:42:16 -0300

    feat: change pagination to show number pages, and pagination when search movies

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,29 +1,64 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 
 import { PaginationProps } from './types'
 import { Wrapper } from './styles'
 
 export function Pagination(props: PaginationProps) {
-  const { loadDiscoverRequest, genre, page, totalPages } = props
+  const {
+    loadDiscoverRequest,
+    loadSearchRequest,
+    genre,
+    page,
+    totalPages,
+    query,
+  } = props
 
-  const handleClick = (pageNumber: number) => {
-    loadDiscoverRequest(pageNumber, genre)
+  const handleClick = (pageNumber: number): void => {
+    loadDiscoverRequest && loadDiscoverRequest(pageNumber, genre)
+    loadSearchRequest && loadSearchRequest(pageNumber, query)
     window.scrollTo(0, 0)
+  }
+
+  const renderPrevious = (): ReactElement | null => {
+    if (page <= 1) return null
+
+    return (
+      <li onClick={() => handleClick(page - 1)}>
+        <button>Anterior</button>
+      </li>
+    )
+  }
+
+  const renderNext = (): ReactElement | null => {
+    if (page >= totalPages) return null
+
+    return (
+      <li onClick={() => handleClick(page + 1)}>
+        <button>Proximo</button>
+      </li>
+    )
+  }
+
+  const renderPageNumbers = (): JSX.Element[] => {
+    let pages: JSX.Element[] = []
+
+    for (let i = page + 1; i <= page + 3 && i < totalPages; i++) {
+      pages.push(
+        <li key={i} onClick={(): void => handleClick(i)}>
+          <button>{i}</button>
+        </li>
+      )
+    }
+
+    return pages
   }
 
   return (
     <Wrapper>
       <ul>
-        {page > 1 && (
-          <li onClick={() => handleClick(page - 1)}>
-            <button>Anterior</button>
-          </li>
-        )}
-        {page < totalPages && (
-          <li onClick={() => handleClick(page + 1)}>
-            <button>Proximo</button>
-          </li>
-        )}
+        {renderPrevious()}
+        {renderPageNumbers()}
+        {renderNext()}
       </ul>
     </Wrapper>
   )
