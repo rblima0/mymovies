f8637af01442874d613f414b01432c2ca9182d54
Rodrigo Banci <rblima0@gmail.com>
2021-01-09 17:34:41 -0300

    Updates

---
+++ precache-manifest.5f58d94f58a761adbbe90b3ec3281ec6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d904299a5907045e71f43dec03ae7700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b61ab1fd65715b42b129",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b61ab1fd65715b42b129",
+    "url": "/static/js/2.03cf87d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.03cf87d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b4926ff46a6d12a456",
+    "url": "/static/js/main.828ef1e6.chunk.js"
+  },
+  {
+    "revision": "eb76c83adc8558bf0e69",
+    "url": "/static/js/runtime-main.4024d48f.js"
+  },
+  {
+    "revision": "bc365d35fb6c8113a1b201d78da5d5fe",
+    "url": "/static/media/popcorn.bc365d35.svg"
+  }
+]);
