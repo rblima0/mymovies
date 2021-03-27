fa16fea2900aaf7ab08b5e44d862344c4a8b8c5c
Rodrigo Banci <rblima0@gmail.com>
2021-03-27 12:05:54 -0300

    Updates

---
+++ precache-manifest.20bd259c11f40c586923f7bf1337ae4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6b6848b98692529db6bdeceb1b39c2",
+    "url": "/mymovies/index.html"
+  },
+  {
+    "revision": "e384e3d8a4bcd6b57bf7",
+    "url": "/mymovies/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e384e3d8a4bcd6b57bf7",
+    "url": "/mymovies/static/js/2.fb82d5da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mymovies/static/js/2.fb82d5da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f480a6d98c52767defc",
+    "url": "/mymovies/static/js/main.b6dbe8b9.chunk.js"
+  },
+  {
+    "revision": "e9de53b55302dbc9f68c",
+    "url": "/mymovies/static/js/runtime-main.8bb3206b.js"
+  },
+  {
+    "revision": "bc365d35fb6c8113a1b201d78da5d5fe",
+    "url": "/mymovies/static/media/popcorn.bc365d35.svg"
+  }
+]);
